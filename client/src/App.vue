101391c23bc01f391c3ab696e97657a4fa2db311
khizerdev <46632721+khizerdev@users.noreply.github.com>
2023-11-10 23:33:09 +0500

    Handling loading and errors

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
     <input type="text" v-model="searchTerm" />
-    <p v-for="book in result?.allBooks" :key="book.id">
-      {{ book.title }}
-    </p>
+    <p v-if="loading">Loading...</p>
+    <p v-else-if="error">Something went wrong! Please try again</p>
+    <template v-else>
+      <p v-for="book in result?.allBooks" :key="book.id">
+        {{ book.title }}
+      </p>
+    </template>
   </div>
 </template>
 
@@ -16,13 +20,11 @@ export default {
   name: "App",
   setup() {
     const searchTerm = ref("");
-    const { result } = useQuery(ALL_BOOKS_QUERY, () => ({
+    const { result, loading, error } = useQuery(ALL_BOOKS_QUERY, () => ({
       search: searchTerm.value,
     }));
 
-    console.log(result);
-
-    return { result, searchTerm };
+    return { result, searchTerm, loading, error };
   },
 };
 </script>
